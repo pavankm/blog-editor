e3c3ce311d933e24cd6983859b7605f28ad631be
Pavan <kpavan@proton.me>
2025-10-18 01:05:25 -0700

    restart

--- src/components/navigation/types.ts
+++
@@ -1,8 +0,0 @@
-import { StyleSheet } from "react-native";
-
-export type Screen = "canvas" | "preview" | "posts" | "settings";
-
-export interface ScreenSelectorProps {
-  currentScreen: Screen;
-  onScreenSelect: (screen: Screen) => void;
-}
--- src/components/toolbar/types.ts
+++
@@ -1,8 +0,0 @@
-export type BrushSize = "small" | "medium" | "large";
-
-export interface BottomToolbarProps {
-  selectedBrushSize: BrushSize;
-  onBrushSizeChange: (size: BrushSize) => void;
-  onClear: () => void;
-  onToolSelect: (tool: string) => void;
-}
